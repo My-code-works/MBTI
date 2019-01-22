4e02e02511a2ff4a0e4346845c8361c6c7743094
Qingsong Lv <lqs@mail.bnu.edu.cn>
2019-01-22 06:57:37 +0000

    build training set

--- model/Preprocessing.py
+++ model/Preprocessing.py
@@ -1,19 +1,56 @@
 import json
 
 class JsonPre:
+    tag2id = {}
+    id2tag = {}
+    @staticmethod
+    def getid(tag):
+        if tag not in JsonPre.tag2id:
+            cnt = len(JsonPre.tag2id)
+            JsonPre.tag2id[tag] = cnt
+            JsonPre.id2tag[cnt] = tag
+            cnt += 1
+        return JsonPre.tag2id[tag]
+    @staticmethod
+    def gettag(id):
+        return JsonPre.id2tag[id]
     @staticmethod
     def section_split(data):
         for js in data:
             del js["simple"]
+            new_full = []
             for i in js["full"]["sectionContents"]:
+                if len(i["text"]) == 0 or i["isContentSection"] == False:
+                    continue
                 i["text"] = i["text"].split('\n')
+                new_full.append(i)
+            js["full"]["sectionContents"] = new_full
     @staticmethod
     def tagging(data):
-        pass
+        for js in data:
+            last = None
+            for i in js["full"]["sectionContents"]:
+                i["tags"] = []
+                for id, x in enumerate(i["text"]):
+                    if last is None:
+                        last = i["depth"]
+                    th = i["depth"] - last
+                    if id == len(i["text"]) - 1:
+                        tag = "E"
+                    else:
+                        tag = "M"
+                    if id == 0:
+                        tag = "B%d"%(th)
+                    i["tags"].append(JsonPre.getid(tag))
+                last = i["depth"]
+
 if __name__ == '__main__':
     with open('data/1.json', 'r', encoding='utf-8') as f:
         text = ''.join(f.readlines())
         data = json.loads(text)
     JsonPre.section_split(data)
-    print(data[0]["full"]["sectionContents"][0])
+    JsonPre.tagging(data)
+    for sec in data[1]["full"]["sectionContents"]:
+        print(sec)
+        input()
 
