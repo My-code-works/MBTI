8dc577008620918b4356845730eeda53a7241e70
Qingsong Lv <lqs@mail.bnu.edu.cn>
2019-01-22 07:15:53 +0000

    add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from model.Preprocessing import JsonPre
+import json
+
+def get_json(fn):
+    with open(fn, 'r', encoding='utf-8') as f:
+        text = ''.join(f.readlines())
+    return json.loads(text)
+
+
+if __name__ == '__main__':
+    num_of_json = 500
+    train = []
+    for i in range(num_of_json):
+        print('loading %d'%(i))
+        data = get_json("data/%d.json"%(i))
+        JsonPre.section_split(data)
+        JsonPre.tagging(data)
+        train += data
+    print(len(train))
