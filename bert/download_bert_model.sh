ce261cede466412d0ef30b76f9fabfdbb24e4557
MarkYu98 <root@ml-instance-yxy98yf.c.ml-winter-camp-pek.internal>
2019-01-23 03:08:38 +0000

    BERT init

---
+++ bert/download_bert_model.sh
@@ -0,0 +1,6 @@
+mkdir bert_model
+cd bert_model
+wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip
+unzip uncased_L-24_H-1024_A-16.zip
+rm uncased_L-24_H-1024_A-16.zip
+
---
+++ bert/set_bert_base.sh
@@ -0,0 +1,2 @@
+export BERT_BASE_DIR=./bert_model/uncased_L-24_H-1024_A-16
+
