51bddedce4557b9a486a3d89e35516947ac6676d
Harrypotterrrr <jiahaolin19971119@126.com>
2019-01-23 15:58:45 +0000

    add bert_model script

---
+++ download_bert_model.sh
@@ -0,0 +1,6 @@
+cd bert
+mkdir bert_model
+cd bert_model
+wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
+unzip uncased_L-12_H-768_A-12.zip
+rm -f uncased_L-12_H-768_A-12.zip
